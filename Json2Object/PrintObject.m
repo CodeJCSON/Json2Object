2b4ba2e420c21a0cc59248278961675aeedb0d70
CodeJCSON <yunmei@yunmeideiMac.local>
2016-12-14 16:20:38 +0800

    Json2Object

---
+++ Json2Object/PrintObject.m
@@ -0,0 +1,117 @@
+//
+//  PrintObject.m
+//  mgen_viewHierarchy
+//
+//  Created by MgenLiu on 13-9-27.
+//  Copyright (c) 2013年 Mgen. All rights reserved.
+//
+
+#import "PrintObject.h"
+#import <objc/runtime.h>
+#define PRINT_OBJ_LOGGING 1
+
+@implementation PrintObject
+
++ (NSDictionary*)getObjectData:(id)obj
+{
+    NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+    unsigned int propsCount;
+    objc_property_t *props = class_copyPropertyList([obj class], &propsCount);
+    for(int i = 0;i < propsCount; i++) {
+        objc_property_t prop = props[i];
+        id value = nil;
+        
+        @try {
+            NSString *propName = [NSString stringWithUTF8String:property_getName(prop)];
+            value = [self getObjectInternal:[obj valueForKey:propName]];
+            if(value != nil) {
+                [dic setObject:value forKey:propName];
+            }
+        }
+        @catch (NSException *exception) {
+            [self logError:exception];
+        }
+        
+    }
+    return dic;
+}
+
++ (void)print:(id)obj
+{
+    NSLog(@"%@", [self getObjectData:obj]);
+}
+
+
++ (NSData*)getJSON:(id)obj options:(NSJSONWritingOptions)options error:(NSError**)error
+{
+    return [NSJSONSerialization dataWithJSONObject:[self getObjectData:obj] options:options error:error];
+}
+
++ (id)getObjectInternal:(id)obj
+{
+    if(!obj
+       || [obj isKindOfClass:[NSString class]]
+       || [obj isKindOfClass:[NSNumber class]]
+       || [obj isKindOfClass:[NSNull class]]) {
+        return obj;
+    }
+    
+    if([obj isKindOfClass:[NSArray class]]) {
+        NSArray *objarr = obj;
+        NSMutableArray *arr = [NSMutableArray arrayWithCapacity:objarr.count];
+        for(int i = 0;i < objarr.count; i++) {
+            [arr setObject:[self getObjectInternal:[objarr objectAtIndex:i]] atIndexedSubscript:i];
+        }
+        return arr;
+    }
+    
+    if([obj isKindOfClass:[NSDictionary class]]) {
+        NSDictionary *objdic = obj;
+        NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithCapacity:[objdic count]];
+        for(NSString *key in objdic.allKeys) {
+            [dic setObject:[self getObjectInternal:[objdic objectForKey:key]] forKey:key];
+        }
+        return dic;
+    }
+    return [self getObjectData:obj];
+}
+
++ (void)logError:(NSException*)exp
+{
+#if PRINT_OBJ_LOGGING
+    NSLog(@"PrintObject Error: %@", exp);
+#endif
+}
+//根据字符串获取集合
++(id)getContainerByData:(NSString*)str
+{
+    NSError *error;
+   id idContainer= [NSJSONSerialization JSONObjectWithData:[str dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:&error];
+    if (error!=nil) {
+        NSLog(@"错误是%@",error);
+    }
+    return idContainer;
+}
++ (id)setObject:(id)obj Data:(NSDictionary *)dic
+{
+    unsigned int propsCount;
+    objc_property_t *props = class_copyPropertyList([obj class], &propsCount);
+    for(int i = 0;i < propsCount; i++) {
+        objc_property_t prop = props[i];
+        id value = nil;
+        
+        @try {
+            NSString *propName = [NSString stringWithUTF8String:property_getName(prop)];
+            value = [dic objectForKey:propName];
+            if(value != nil) {
+                [obj setValue:value forKey:propName];
+            }
+        }
+        @catch (NSException *exception) {
+            [self logError:exception];
+        }
+        
+    }
+    return obj;
+}
+@end
